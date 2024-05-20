39a67df89b301fb3e5ed2ffc74aecfc47f676761
ToastedToast <50563138+ToastedDev@users.noreply.github.com>
2024-05-20 21:13:18 +0800

    feat: add api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello World!");
+});
+
+const port = parseInt(process.env.PORT || "3000");
+export default {
+  fetch: app.fetch,
+  port,
+};
